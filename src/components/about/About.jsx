6794e4bac6de370245c407b65d427909f2a8f579
HugoBraj <hugobrajdic@gmail.com>
2026-02-01 15:47:15 +0100

    Dynamic age

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,12 +3,25 @@ import "./about.css"
 import Hugo from '../../assets/Hugo.png'
 
 const About = () => {
+  const getAge = (birthDate) => {
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  const birthDate = new Date(2003, 0, 5);
+  const age = getAge(birthDate);
+
   return (<section className='container' id='about'>
     <h2>À propos</h2>
     <div className="about__main">
       <img src={Hugo} alt='Hugo Brajdic'></img>
       <div className="about__intro">
-        Je m'appelle Hugo et j'ai 21 ans. Je suis passionné par la création informatique. <br></br>
+        Je m'appelle Hugo et j'ai {age} ans. Je suis passionné par la création informatique. <br></br>
         <div className="sub-text">
         La programmation, le graphisme ou encore la modélisation 3D sont mes terrains de jeu favoris. <br></br>
         Je suis étudiant en BUT Informatique et employé en alternance pour le groupe Covéa. <br></br>
